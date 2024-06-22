388b482146d6cf8d495f6dae3720a02c3fcd3655
chaponanwill <chapowilliam09@gmail.com>
2024-06-21 22:30:31 -0500

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site:'https://mirelyLaboriano.github.io',
-  base:'clone-pw'
+  site:'https://MirelyLaboriano.github.io',
+
+base:'clone-pw',
 });
